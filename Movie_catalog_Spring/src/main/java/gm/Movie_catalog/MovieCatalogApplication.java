6156616bf9b42917512a0e2b70641b8d36b00645
Reisorz <miguelminib@gmail.com>
2024-07-23 19:24:23 +0200

    Methods elegirOpcion, realizarAccion & listarPeliculas from MovieCatalogoApplication Class done.

--- Movie_catalog_Spring/src/main/java/gm/Movie_catalog/MovieCatalogApplication.java
+++ Movie_catalog_Spring/src/main/java/gm/Movie_catalog/MovieCatalogApplication.java
@@ -1,5 +1,9 @@
 package gm.Movie_catalog;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +11,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import gm.Movie_catalog.modelo.Pelicula;
 import gm.Movie_catalog.servicio.IPeliculaServicio;
 
 @SpringBootApplication
@@ -18,6 +23,8 @@ public class MovieCatalogApplication implements CommandLineRunner{
 	private static final Logger logger = LoggerFactory.getLogger(MovieCatalogApplication.class);
 
 	String nl = System.lineSeparator();
+	String nl2 = System.lineSeparator() + System.lineSeparator();
+	
 
 	public static void main(String[] args) {
 		logger.info("Iniciando la aplicacion");
@@ -30,8 +37,77 @@ public class MovieCatalogApplication implements CommandLineRunner{
 		movieCatalogApp();
 	}
 
+
 	private void movieCatalogApp(){
-		logger.info(nl + "*** Aplicacion Movie Catalog ***" + nl);
+		logger.info(nl2 + "*** Aplicacion Movie Catalog ***" + nl);
+		int opcion = 0;
+
+		while (opcion !=6) {
+			opcion = elegirOpcion();
+			realizarAccion(opcion);
+		}
+		
+	}
+
+	private int elegirOpcion() {
+		Scanner scan = new Scanner(System.in);
+		
+		logger.info(nl + "Elija una opción: " + nl + 
+		"1 - Listar películas." + nl + 
+		"2 - Añadir película." + nl + 
+		"3 - Modificar película." + nl +
+		"4 - Buscar película." + nl +
+		"5 - Borrar película." + nl +
+		"6 - Cerrar aplicación." + nl + 
+		"Opcion: ");
+		int opcion = Integer.parseInt(scan.nextLine());
+		return opcion;
+	}
+
+	private void realizarAccion (Integer opcion) {
+		switch (opcion) {
+            case 1:
+				List<Pelicula> peliculas = listarPeliculas();
+				logger.info(nl + "Películas: " + nl);
+				for (Pelicula pelicula : peliculas) {
+					logger.info(pelicula.toString());
+				}
+                break;
+            
+            case 2:
+                
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+
+            case 5:
+                
+                break;
+
+            case 6:
+                
+                System.out.println(nl + "Fin del programa." + nl);
+                break;
+        
+            default:
+
+                System.out.println("Inserta una opción válidad.");
+                elegirOpcion();
+                break;
+        }
+
+	}
+
+	private List<Pelicula> listarPeliculas () {
+		List<Pelicula> peliculas = new ArrayList<>();
+		peliculas = peliculaServicio.listarPeliculas();
+		return peliculas;
 	}
 
 }
